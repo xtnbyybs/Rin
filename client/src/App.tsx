1665da9cecf4470fe2fe3c18c49ea1cf78cb1bbc
xtang <2111803140@qq.com>
2026-06-12 20:22:59 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,21 @@ import { AppProviders } from "./app/providers";
 import { AppRoutes } from "./app/routes";
 import { useAppBootstrap } from "./app/use-app-bootstrap";
 
+// 1. 引入你刚才创建的音乐播放器组件
+// 🚨 注意：请确保这里的相对路径与你实际存放文件的位置一致
+// 假设你把它放在了同级的 components 文件夹里：
+import { MusicPlayer } from "./components/MusicPlayer"; 
+
 function App() {
   const { config, profile } = useAppBootstrap();
 
   return (
     <AppProviders config={config} profile={profile}>
+      {/* 博客的所有页面内容 */}
       <AppRoutes />
+      
+      {/* 2. 把音乐播放器挂载在这里，作为全局悬浮组件 */}
+      <MusicPlayer />
     </AppProviders>
   )
 }
