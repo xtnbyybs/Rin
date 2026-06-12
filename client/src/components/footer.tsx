d76d20c11a2bce990938e49618fb47ee41c8b753
xtang <2111803140@qq.com>
2026-06-12 12:53:29 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -102,7 +102,7 @@ function Footer() {
                             setDoubleClickTimes(doubleClickTimes + 1)
                         }
                     }}>
-                        © {new Date().getFullYear()} Powered by <a className='hover:underline' href="https://915161.xyz" target="_blank">汤</a>
+                        © {new Date().getFullYear()} Powered by <a className='hover:underline' href="https://915161.xyz" target="_blank">Rin / 汤</a>
                     </span>
                     {config.getBoolean('rss') && <>
                         <Spliter />
