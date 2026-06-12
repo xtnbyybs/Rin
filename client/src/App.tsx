4e72b84b5ce9552e4b3bc7d8e235e4d9718c7b25
xtang <2111803140@qq.com>
2026-06-12 19:16:53 +0800

    更新 App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
+
 import { AppProviders } from "./app/providers";
 import { AppRoutes } from "./app/routes";
 import { useAppBootstrap } from "./app/use-app-bootstrap";
+import { MusicPlayer } from '@/components/MusicPlayer';
 
-function App() {
+function AppContent() {
   const { config, profile } = useAppBootstrap();
-
   return (
     <AppProviders config={config} profile={profile}>
       <AppRoutes />
@@ -12,4 +13,17 @@ function App() {
   )
 }
 
-export default App
+export default function RootLayout({ children }) {
+  return (
+    <html lang="zh">
+      <body>
+        {/* 原有应用主体 */}
+        <AppContent />
+        {children}
+
+        {/* 全局播放器，置于页面最底部 */}
+        <MusicPlayer />
+      </body>
+    </html>
+  );
+}
